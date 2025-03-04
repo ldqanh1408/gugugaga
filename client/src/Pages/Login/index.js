33c8d8cd0de9cf938e44a0eae6871ef0a5af535a
BuBaKaNg <n23dccn164@student.ptithcm.edu.vn>
2025-03-04 18:38:12 +0700

    THanh canlendar

--- client/src/Pages/Login/index.js
+++ client/src/Pages/Login/index.js
@@ -1,38 +1,7 @@
-import React, {useState} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Form } from "react-bootstrap";
-import { getUsers } from '../../services/userService.js'
 import './Login.css'
-import {useNavigate} from 'react-router-dom'
 function Login(){
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const users = await getUsers();
-          const user = users.find((user, index) => user.user_name === userName && user.password === password);
-          if(user){
-             // Giả sử bạn có một token giả lập để lưu vào localStorage
-            const token = 'fake-jwt-token';
-            localStorage.setItem('token', token);
-            // Chuyển hướng người dùng đến trang khác
-            console.log('Login successful:', user);
-            navigate('/');
-            window.location.reload();
-          }
-          // Lưu token vào localStorage hoặc state quản lý
-            // Chuyển hướng người dùng đến trang khác 
-            else {
-                setError('Invalid username or password');
-            }
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            setError('An error occurred. Please try again.');
-        }
-      };
     return(
         <div className="containter form-1">
             <div className="title">
@@ -42,18 +11,16 @@ function Login(){
                 <Form className="d-flex flex-column form-2">
                     <Form.Group>
                         <Form.Label className="fw-semibold">User name:</Form.Label>
-                        <Form.Control placeholder="Nhập tên tài khoản..." className="form-control" onChange={(e) => setUserName(e.target.value)}></Form.Control>
+                        <Form.Control placeholder="Nhập tên tài khoản..." className="form-control"></Form.Control>
                         <Form.Text></Form.Text>
                     </Form.Group>
                     <Form.Group className="mt-4">
                         <Form.Label className="fw-semibold">Password:</Form.Label>
-                        <Form.Control placeholder="Nhập mật khẩu..." className="form-control" onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                        <Form.Control placeholder="Nhập mật khẩu..." className="form-control"></Form.Control>
                         <Form.Text></Form.Text>
                     </Form.Group>
                 </Form>
-                <Form>
-                    <Button type="submit" onClick={handleSubmit}>Login</Button>
-                </Form>
+                <Button>Login</Button>
             </div>
         </div>
     );
